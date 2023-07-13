0ffc544346d875e2cc54e568a1f762162366a888
ArtemRa <artem.rakhimzhanov@iwildpartners.com>
2023-07-13 21:25:13 +0300

    Init project

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import BaseTable from "../BaseTable/BaseTable";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <BaseTable />
+    </div>
+  );
+}
+
+export default App;
